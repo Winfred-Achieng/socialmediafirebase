eb7b27a8a87c19909bb63e172524a0c10a135f9c
Winfred-Achieng <achiengwinfred20@gmail.com>
2024-11-03 13:11:05 +0300

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import './App.css';
+import {Main} from './pages/main'
+import {Login} from './pages/login'
+import {Navbar} from './components/navbar'
 
 function App() {
   return (
@@ -8,10 +11,13 @@ function App() {
       
       <Router>
 
-        <Routes>
-          <Route path="/" element={} />
+        <Navbar/>
 
-        </Routes>
+        <Routes>
+          <Route path="/" element={<Main/>} />
+          <Route path="/login" element={<Login/>} />
+
+          </Routes>
       </Router>
 
 
